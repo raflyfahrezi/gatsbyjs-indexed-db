34fe4fb633244b21f8592365d10ce473183fd8dd
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-09-06 13:48:10 +0700

    gagal library nya ga support server side rendering jadi gabisa pake gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
 //library
 import React, { useState } from 'react'
-import { initDB } from 'react-indexed-db'
+// import { initDB } from 'react-indexed-db'
 
 //indexed db
 import { dbconfig } from '../../dbconfig'
 
 //component
 import Title from '../../component/Title/Title'
-import AddingPeople from '../../component/AddingPeople/AddingPeople'
-import RetrievePeople from '../../component/RetrievePeople/RetrievePeople'
+// import AddingPeople from '../../component/AddingPeople/AddingPeople'
+// import RetrievePeople from '../../component/RetrievePeople/RetrievePeople'
 import Head from '../../component/Head/Head'
 
 //initialize db
-initDB(dbconfig)
+// initDB(dbconfig)
 
 const Index = () => {
     const [ isUpdate, setIsUpdate ] = useState(0)
@@ -21,8 +21,8 @@ const Index = () => {
         <div>
             <Head title='React with Indexed DB'/>
             <Title title='React with Indexed DB'/>
-            <AddingPeople isUpdate={ isUpdate } setIsUpdate={ e => setIsUpdate(e) }/>
-            <RetrievePeople isUpdate={ isUpdate }/>
+            {/* <AddingPeople isUpdate={ isUpdate } setIsUpdate={ e => setIsUpdate(e) }/>
+            <RetrievePeople isUpdate={ isUpdate }/> */}
         </div>
     )
 }
